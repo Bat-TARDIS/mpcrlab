336ce501b8904c5ee6d453538d3c724bc2ea92e5
Yunier <bleachrulez@aim.com>
2015-03-16 15:15:46 -0400

    update file

--- rover_test.py
+++ rover_test.py
@@ -1,35 +1,17 @@
+import cv2
+import StringIO
 from rover import Rover20
 
-import time
-import pygame
-import sys
-import signal
-import cv2
+class Tank(Rover20):
+        def processVideo(self, jpegbytes):
 
-class roverMain(Rover20):
-
-    def __init__(self):
-        Rover20.__init__()
-        self.window_name = "Rover 2.0 Hit Esc to Quit"
-        self.quit = False
-
-        try:
             if cv2:
-                cv2.namedWindow(self.window_name, cv2.CV_WINDOW_AUTOSIZE)
+                image = StringIO.StringIO(jpegbytes)
+                wname = 'Rover 2.0'
+                cv2.NamedWindow(wname, cv2.CV_WINDOW_AUTOSIZE )
+                cv2.ShowImage(wname, image )
+                return image
+                cv.WaitKey(5)
             else:
                 pass
-        except:
-            pass
-
-    def video(self, jpegbytes, timestamp_10msec):
-        image = cv2.VideoCapture(0)
-
-        if cv2.waitKey(1) & 0xFF ==27 :
-            self.quit = True
-
-
-
-
-
-
 
